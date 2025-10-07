0386b55973a7a9b6c2051832ba6ae4ff1f973afc
Safoun <safoun.official@gmail.com>
2025-10-07 12:02:03 +0600

    2 upsolving done

---
+++ upsolving_phitron_oj/N Times/times.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned short times;
+    scanf("%hu", &times);
+    for (unsigned short i = 1; i <= times; i++)
+    {
+        printf("I Love Practice\n");
+    }
+    return 0;
+}
