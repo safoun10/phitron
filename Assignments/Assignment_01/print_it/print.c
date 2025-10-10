c6e6419cf11787ff6f6f9a749aa17f1d91cc357c
Safoun <safoun.official@gmail.com>
2025-10-10 09:04:45 +0600

    added and solved first problem of first assignment

---
+++ Assignments/Assignment_01/print_it/print.c
@@ -0,0 +1,16 @@
+/*
+Hello, world! I am learning C programming language. ^_^
+Programming is fun and challenging. /\/\/\
+I want to give my 100% dedication to learn!	I will succeed one day.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, world! I am learning C programming language. ^_^\n");
+    printf("Programming is fun and challenging. ");
+    printf("/\\/\\/\\\n");
+    printf("I want to give my 100%% dedication to learn!\tI will succeed one day.");
+    return 0;
+}
