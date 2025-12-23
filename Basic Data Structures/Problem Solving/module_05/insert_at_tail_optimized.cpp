0c1aa00d3f397bba333287ab86b0b229cbddb46d
Safoun <safoun.official@gmail.com>
2025-12-23 16:24:35 +0600

    learning singly linked list operation

---
+++ Basic Data Structures/Problem Solving/module_05/insert_at_tail_optimized.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+
+    head->next = a;
+    a->next = b;
+    b->next = tail;
+
+    insert_at_tail(head, tail, 50);
+    insert_at_tail(head, tail, 60);
+    print_linked_list(head);
+
+    return 0;
+}
